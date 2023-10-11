947bfc53fca859583000a786b19aea880b5f197f
NkosiT <thokoterris@gmail.com>
2023-10-11 07:38:19 -0700

    Edited the weather forecast

--- src/app.js
+++ src/app.js
@@ -21,6 +21,33 @@ function formatDate(timestamp) {
 	return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+	let forecastElement = document.querySelector("#forecast");
+	
+	let days = ["Thu", "Fri", "Sat", "Sun"];
+	
+	let forecastHTML = `<div class="row">`;
+	days.forEach(function(day) {
+	forecastHTML = forecastHTML +
+    `
+              <div class="col-2">
+				<div class="weather-forecast-date">${day}</div>
+                
+				 <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="" width="36" />
+				 <div class="weather-forecast-temperatures"></div>
+                  <span class="weather-forecast-temperatures-max">
+					18°
+				  </span>
+				  <span class="weather-forecast-temperatures-min">
+					12°
+				  </span>
+			  </div>
+		`;	
+    });
+		forecastHTML = forecastHTML + `</div>`;
+		forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response){
 	let temperatureElement = document.querySelector("#temperature");
 	let cityElement = document.querySelector("#city");
@@ -80,3 +107,4 @@ let celsiusTemperature = null;
    
  
    search("New York");
+   displayForecast()
